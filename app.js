0471e5dca7ea80f6001404fa2e19012e94bb59aa
iroh99 <iroh99@protonmail.com>
2020-08-25 13:11:49 +0200

    added controllers and testing

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("express-async-errors");
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const errorHandler = require("./utils/middleware").errorHandler;
 //const middleware = require("./utils/middleware");
 //app.use(express.static());
 
@@ -14,13 +18,15 @@ mongoose
   .connect(config.URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     logger.info("connected to MongoDB");
-  })
-  .catch((error) => {
-    logger.error("error connection to MongoDB:", error.message);
   });
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/blogs", blogsRouter);
+app.use("/blogs", blogsRouter);
+app.use("/users", usersRouter);
+app.use("/login", loginRouter);
+app.use(errorHandler);
 
 module.exports = app;
+
+//The ref option is what tells Mongoose which model to use during populatio
